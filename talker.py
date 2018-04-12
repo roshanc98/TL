a91791337a760462a0fc93a848c45b328fbdc68e
Roshan <roshanc98@gmail.com>
2018-04-12 12:23:52 +0530

    reinitialized files

---
+++ talker.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import rospy
+import time
+from std_msgs.msg import String
+
+def talker():
+    pub = rospy.Publisher('chatter', String, queue_size=10)
+    rospy.init_node('talker', anonymous=True)
+    rate = rospy.Rate(10) # 10hz
+    while not rospy.is_shutdown():
+	localtime = time.asctime( time.localtime(time.time()) )
+	print "The time is :", localtime
+        hello_str = "hello world %s" % rospy.get_time()
+        rospy.loginfo(hello_str)
+        pub.publish(hello_str)
+        rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        talker()
+    except rospy.ROSInterruptException:
+        pass
